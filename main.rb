9a7d99307375c9f41d55a77cf35b329ef006ad17
Chiyo T N <chiyo.nishio@gmail.com>
2019-05-09 13:05:36 -0700

    Fix TODOs and make (not-so-smart) AI work

--- main.rb
+++ main.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby
 
 class Game
+  VALUE_HUMAN = "X"
+  VALUE_AI    = "O"
+
   def initialize
     @board = Array.new(3) { Array.new(3) }
-    @is_x = true
     @wins = [
       [[0,0], [0,1], [0,2]],
       [[1,0], [1,1], [1,2]],
@@ -14,94 +16,92 @@ class Game
       [[0,0], [1,1], [2,2]],
       [[0,2], [1,1], [2,0]],
     ]
-
     @vicinity = [
+      [-1,-1],
       [-1,0],
-      [1,0],
+      [-1,1],
       [0,-1],
       [0,1],
-      [-1,-1],
+      [1,-1],
+      [1,0],
       [1,1]
     ]
   end
 
-  # TODO: validate the input value from STDIN
-  # - that it's an integer
-  # - that it's not already taken
   def place(location)
-    row, col = location.split(",")
-    # @board[row.to_i][col.to_i] = @is_x ? "X" : "O"
-    # if @is_x
-    #   @board[row.to_i][col.to_i] = "X"
-    # else
-    #   @board[row.to_i][col.to_i] = "O"
-    # end
-    @board[row.to_i][col.to_i] = "X"
-    computer_place()
-    @is_x = !@is_x
+    row, col = location.split(",").map(&:to_i)
+    if (!row.between?(0, 2)) || (!col.between?(0, 2))
+      puts 'Row and col have to be between 0 and 2!'
+      return false
+    end
+    if !@board[row][col].nil?
+      puts 'Location already taken!'
+      return false
+    end
+
+    @board[row.to_i][col.to_i] = VALUE_HUMAN
+    true
   end
 
-  # TODO: isolate method to check emptiness of neighboring locations
-  # TODO: better name for "location"
-  def computer_place
+  def ai_place
+    puts "[AI] Hmm..."
+    sleep 0.2
+    row, col = @board.find(VALUE_AI)
+    unless row != nil
+      @vicinity.each do |delta|
+        trial_row = row + delta[0]
+        trial_col = col + delta[1]
+
+        if @board[trial_row][trial_col]&.nil?
+          @board[trial_row][trial_col] = VALUE_AI
+          return
+        end
+      end
+    end
+
+    row, col = find(nil)
+    @board[row][col] = VALUE_AI
+  end
+
+  def find(target)
     @board.each_with_index do |row, i|
       row.each_with_index do |value, j|
-        if value == "O"
-          if @board[i][j-1].nil?
-            @board[i][j-1] == "0"
-          else if @board[i][j-1].nil?
-
-        end
+        return i, j if value == target
       end
+    end
+
+    return nil
+  end
+
+  def is_done
+    if is_win(VALUE_HUMAN)
+      puts "You won!"
+      return true
+    elsif is_win(VALUE_AI)
+      puts "AI won!"
+      return true
+    end
+
+    if @board.find(nil)
+      false
+    else
+      puts "It's a tie!"
+      true
     end
   end
 
-  # TODO: print result to console
-  def is_done
-    # TODO: create a method to determine if X/O has won
-    # check if X has won
+  def is_win(value)
     @wins.each do |win|
-      all_three_is_x = true
-      win.each do |location|
-        if @board[location[0]][location[1]] != "X"
-          all_three_is_x = false
-        end
-      end
-
-      return true if all_three_is_x
+      check = win.map{|location| @board[location[0]][location[1]] == value }
+      return true if check.all? true
     end
-    # check if O has won
-    @wins.each do |win|
-      all_three_is_x = true
-      win.each do |location|
-        if @board[location[0]][location[1]] != "O"
-          all_three_is_x = false
-        end
-      end
-
-      return true if all_three_is_x
-    end
-    # TODO: simplify logic
-    # check if it's a tie
-    is_full = true
-    @board.each do |row|
-      row.each do |value|
-        if value.nil?
-          is_full = false
-          break
-        end
-      end
-    end
-
-    return is_full
+    false
   end
 
   def print_board
     @board.each do |row|
-      row.each do |value|
-        print("|#{value}|")
-      end
-      print("\n")
+      row.each {|value| print "|#{value}|" }
+      print "\n"
     end
   end
 end
@@ -109,7 +109,16 @@ end
 game = Game.new
 game.print_board
 while !game.is_done
-  location = $stdin.gets()
-  game.place(location)
+  loop do
+    print "Your turn. Enter row,col: "
+    location = $stdin.gets()
+    break if game.place(location)
+  end
   game.print_board
+  if game.is_done
+    break
+  else
+    game.ai_place
+    game.print_board
+  end
 end
